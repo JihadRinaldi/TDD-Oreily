132991c4ae4781e4cce5807f4c4c8825e193b150
Muhammad Jihad Rinaldi <jihadmjr@gmail.com>
2017-11-30 20:07:47 +0700

    edit ignore, and add more test

--- test.py
+++ test.py
@@ -1,6 +1,26 @@
 from selenium import webdriver
+from django.test import TestCase
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# browser = webdriver.Firefox()
+# browser.get('http://localhost:8000')
+#
+# assert 'Todo' in browser.title
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_getaccess_and_retrieve(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('to', self.browser.title)
+        # self.fail('try again')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
